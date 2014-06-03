5e0e29a019b016c38b8d50849b06269c64cb43df
Galder Zamarreño <galder@zamarreno.com>
2014-06-03 10:57:55 +0200

    Update for JUG Switzerland (Basel) w/ Java 8 code for Java verticle

--- reactive-vert.x/demos/java-verticle/HelloWorld.java
+++ reactive-vert.x/demos/java-verticle/HelloWorld.java
@@ -4,10 +4,8 @@ import org.vertx.java.platform.Verticle;
 
 public class HelloWorld extends Verticle {
   public void start() {
-    vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest req) {                
-        req.response().end("<body><h1>Hello World from Java!</h1></body>");
-      }
-    }).listen(8080);
+     vertx.createHttpServer().requestHandler(req ->
+           req.response().end("<body><h1>Hello World from Java!</h1></body>")
+     ).listen(8080);
   }
 }
